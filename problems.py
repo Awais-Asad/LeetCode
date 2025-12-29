b3206a1d13c014d7b013ce4ddc74779c202b9ee8
Awais-Asad <awaisalich1@gmail.com>
2025-12-29 21:39:16 +0500

    13. Roman to Integer

--- problems.py
+++ problems.py
@@ -23,3 +23,23 @@ class Solution:
                 return False
         return True
     
+    # 13. Roman to Integer
+    def romanToInt(self, s: str) -> int:
+        symbol_mapping = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        sum=0
+        for i in range(len(s)-1):
+            if (symbol_mapping[s[i]] >= symbol_mapping[s[i+1]]):
+                sum = sum + symbol_mapping[s[i]]
+            else:
+                sum = sum - symbol_mapping[s[i]]
+        sum = sum + symbol_mapping[s[len(s)-1]]
+        return sum
+    
